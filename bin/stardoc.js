7f81af87daaa5db3306d69411c75aca0ae403bc5
Esseb <esseb@esseb.com>
2014-04-27 12:49:33 +0200

    Move style parser code to parser.js

--- bin/stardoc.js
+++ bin/stardoc.js
@@ -113,103 +113,6 @@ function findStyleFiles(root, styleIgnore) {
 }
 
 /**
- * Extract style comments from the given style string. A style guide comment is
- * a multiline comment that begins with /** ala JSDoc.
- * 
- * @param {String} style The content of a style file.
- * @return {Array}
- */
-function readComments(style) {
-  // This regex will fail if any content strings include /** or */.
-  var comments = style.match(/(\/\*\*[\s\S]*?\*\/)/gm);
-
-  if (!comments) {
-    return [];
-  }
-
-  comments.forEach(function (comment, i) {
-    comments[i] = cleanedComment(comment);
-  });
-
-  return comments;
-
-  // TODO: Ignore /***+ https://github.com/jsdoc3/jsdoc/issues/215
-}
-
-/**
- * Strip leading and trailing whitespace, the comment start and end tags, and
- * the leading asterisk per line if there are any.
- *
- * @param {String} comment
- * @return {String} The cleaned comment.
- */
-function cleanedComment(comment) {
-  // https://github.com/jsdoc3/jsdoc/issues/215#issuecomment-9389993
-
-  var lines = [];
-
-  comment = comment.split(/\r\n|\n/);
-  comment.forEach(function (line) {
-    // Skip start/end tag lines entirely if there is nothing else on that line.
-    if (line.match(/^\s*(\/\*\*|\*\/)\s*$/)) {
-      return true;  // continue
-    }
-
-    // TODO: This will fail if any line inside the multiline comment begins
-    // with /**. A proper parser is required to fix this.
-    line = line.trim();
-    line = line.replace(/^\*\s*/, '');    // Leading asterisk.
-    line = line.replace(/^\/\*+/, '');    // Comment start tag.
-    line = line.replace(/\*\/\s*$/, '');  // Comment end tag.
-    line = line.trim();
-    lines.push(line);
-  });
-
-  return lines.join('\n');
-}
-
-/**
- * Parse a style guide comment and return a style object containing its
- * description and parameters. Parameters with no value are set to "true".
- *
- * @param {String} comment
- * @return {Object}
- */
-function parsedComment(comment) {
-  // The description is everything up to the first line beginning with an @.
-  var description = comment.match(/([\s\S]+?)(\r\n|\n)@/);
-  if (description) {
-    description = description[1].trim();
-  }
-
-  // Extract parameters, supporting values that span multiple lines.
-  var parameters = {};
-  var currentParameter;
-  comment.split(/\r\n|\n/).forEach(function (line) {
-    if (/^@/.test(line)) {
-      var tokens = line.match(/^@(\w+)\s*(.+)?/);
-      currentParameter = tokens[1];
-      parameters[currentParameter] = tokens[2] ? tokens[2] : true;
-    }
-    else {
-      if (currentParameter) {
-        parameters[currentParameter] += ' ' + line;
-      }
-    }
-  });
-
-  if (!description && !parameters) {
-    return;
-  }
-
-  return {
-    originalDescription: description,
-    description: markdown.toHTML(description),
-    params: parameters
-  };
-}
-
-/**
  * Read the markup for the given style object and store it.
  *
  * @param {String} markupFolder The markup root folder.
@@ -221,7 +124,7 @@ function getMarkup(markupFolder, style) {
   }
 
   var markupPath = path.join(
-      markupFolder, path.dirname(style.path), style.params.markup);
+      markupFolder, path.dirname(style.stylePath), style.params.markup);
   
   if (!fs.existsSync(markupPath)) {
     throw new Error(markupPath + ' does not exist');
@@ -231,7 +134,7 @@ function getMarkup(markupFolder, style) {
     throw new Error(markupPath + ' is not a file');
   }
 
-  style.markup = fs.readFileSync(markupPath, 'utf8');
+  return fs.readFileSync(markupPath, 'utf8');
 }
 
 /**
@@ -310,20 +213,14 @@ function run(options) {
   var styleObjects = [];
 
   styleFiles.forEach(function (file) {
-    var styleContent = fs.readFileSync(file, 'utf8');
-    var styleComments = readComments(styleContent);
+    var stylePath = path.relative(options.styleFolder, file);
+    styleObjects = styleObjects.concat(
+        parser.parseStyleFile(options.styleFolder, stylePath));
+  });
 
-    styleComments.forEach(function (comment) {
-      var styleObject = parsedComment(comment);
-      if (!styleObject) {
-        return true;  // continue
-      }
-
-      styleObject.path = path.relative(options.styleFolder, file);
-      getMarkup(options.markupFolder, styleObject);
-
-      styleObjects.push(styleObject);
-    });
+  styleObjects.forEach(function (styleObject) {
+    var markup = getMarkup(options.markupFolder, styleObject);
+    styleObject.markup = markup;
   });
 
   styleObjects = categorizeStyleObjects(styleObjects);
@@ -334,6 +231,7 @@ function run(options) {
   mkdirp.sync(options.outputFolder);
 
   // Copy the template's static folder over.
+  // TODO: Only if stardoc.json specifies a static folder.
   ncp(
       path.join(options.templateFolder, 'static'),
       path.join(options.outputFolder, 'static'),
