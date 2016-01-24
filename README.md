14cbcff1d267364f55ae31699761803d614e2e1e
Stein Cato Blostrupmoen <esseb@esseb.com>
2016-01-24 23:47:07 +0100

    Rewrite

--- README.md
+++ README.md
@@ -1,32 +1,109 @@
-Stardoc
-=======
+# Stardoc
 
-Test usage:
-* Install NodeJS
-* Navigate to the `stardoc` folder
-* `npm install`
-* Stardoc can now be executed by calling `node STARDOC_FOLDER\bin\stardoc.js`
+Stardoc comments begin with `/**` and end with ` */`. All lines within the
+stardoc comment currently must start with ` *`. The parser is a little
+unforgiving for now.
 
-Example project:
-* Navigate to the `files` folder
-* `npm install`
-* Add/edit `.less` files in the `less` folder
-* `grunt less` or `grunt watch` to compile the less files if needed
-* Run `stardoc.js` from the `files` folder
+The first section of a Stardoc comment, up to the first parameter, is the
+style's description. Descriptions are optional.
 
-Styleguide comments begin with /**. The first string inside the comment block
-is the style's description. The publish function in the example project parses
-this description as markdown.
+Parameters follow the description and begin with an `@`.
 
-Parameters follow the description and start with an `@`. Supported parameters
-at the moment are `title`, `id`, `parent`, and `markup`:
+Supported parameters at the moment are `title`, `id`, and `markup`:
 * `title` is the title used to refer to the style object.
-* `id` is the `category` and `name` of the style object, separate with a /.
-  Valid characters are [_a-zA-Z0-9-]+
-  The `category` is used to collect various style objects together. For example
-  'module' and 'component', or 'quark' and 'atom', or 'form' and 'layout'.
-* `parent` is used for styles that modifies another style object. The string
-  must match the `name` part of an `id` parameter of a style object with the
-  same category.
-* `markup` is the filename of a file containing only the markup needed to test
-  the CSS for the style.
+* `id` is the `category` and `name` of the style object, separated with a `/`.
+  * **required**
+* `markup` is the path to a file containing only the markup needed to test
+  the CSS for the style. The path should be relative to the location of the CSS
+  file.
+
+Stardoc is not meant to be used alone. Stardoc passes the data it collects
+along to the given generator. No single generator could fit all projects that
+need style documentation. Stardoc users are instead expected to create their
+own, custom, generators, to fit their needs. The example generators, published
+with an MIT license, can be used as a starting point.
+
+## Usage
+```
+var stardoc = require('stardoc');
+var generator = require('stardoc-generator-example');
+
+var generatorOptions = {
+  title: 'Foobar Styleguide',
+  template: {
+    main: 'templates/main.html'
+  }
+};
+
+var stardocOptions = {
+  include: 'css/**/*.less',
+  generator: generator(generatorOptions)
+}
+
+stardoc(stardocOptions, function(err) {
+  // Handle error here.
+});
+```
+
+## Example
+### main.css
+```
+/**
+ * Small, stand-alone, elements. Generally used for single elements with no
+ * children.
+ *
+ * @title Atoms
+ * @id atoms
+ */
+@import "atoms/button";
+@import "atoms/";
+
+/**
+ * Larger elements with more complex markup than atoms. Can contain atoms.
+ *
+ * @title Modules
+ * @id Modules
+ */
+@import "modules/breadcrumb";
+@import "modules/comment";
+```
+
+### atoms/button.css
+```
+/**
+ * Standard button menu.
+ *
+ * @title Button
+ * @id modules/button
+ * @markup button/button.html
+ */
+.button {
+  background-color: aliceblue;
+  border: 0;
+  color: black;
+  margin: 0;
+  padding: 0;
+}
+
+/**
+ * Button variant to use when indicating that a button's actions perform a
+ * dangerous action.
+ *
+ * @title Button ()
+ * @id modules/button/button--danger
+ * @markup button/button--danger.html
+ */
+.button--danger {
+  background-color: red;
+}
+```
+
+### modules/button/button.html
+```
+<button class="button">Button</button>
+```
+
+### modules/button/button--danger.html
+```
+<button class="button button--danger">Button</button>
+```
---
+++ stardoc.js
@@ -0,0 +1,259 @@
+module.exports = stardoc;
+
+var async = require('async');
+var fs = require('fs');
+var glob = require('glob');
+var path = require('path');
+
+function stardoc(options, cb) {
+  // TODO(scb): Verify options object.
+  // options.include
+  // options.exclude
+  // options.generator
+
+  glob(options.include, (err, files) => {
+    if (err) {
+      console.log('error', err);
+      return;
+    }
+
+    async.map(files, getStardocBlocks, (err, results) => {
+      if (err) {
+        cb(err);
+        return;
+      }
+
+      var stardocBlocks = [];
+      results.forEach(result => {
+        if (result) {
+          stardocBlocks = stardocBlocks.concat(result);
+        }
+      });
+
+      var object = createStardoc(stardocBlocks);
+      options.generator(object.children);
+    });
+  });
+}
+
+function createStardoc(stardocBlocks) {
+  var stardocObject = {};
+
+  stardocBlocks.forEach(block => {
+    createStardocObject(stardocObject, {
+      id: block.parameters.id,
+      title: block.parameters.title,
+      description: block.description,
+      markup: block.markup
+    });
+  });
+
+  return stardocObject;
+}
+
+/**
+ * Return an object from the stardoc object matching the given id, creating the
+ * object, along with any parents, if it does not already exist.
+ *
+ * @param {Object} stardocObject
+ * @param {Object} options
+ */
+function createStardocObject(stardocObject, options) {
+  var tokens = options.id.split('/');
+
+  var object = stardocObject;
+  for (var i = 0; i < tokens.length; i++) {
+    var key = tokens[i];
+
+    var children = object.children = object.children || [];
+    var child = children.find(child => {
+      return child.key === key;
+    });
+
+    if (child) {
+      object = child;
+      continue;
+    }
+
+    var object = {
+      key: key
+    };
+
+    children.push(object);
+
+    children.sort((a, b) => {
+      if (a.key < b.key) {
+        return -1;
+      }
+
+      if (a.key > b.key) {
+        return 1;
+      }
+
+      return 0;
+    });
+  }
+
+  object.title = options.title || null;
+  object.description = options.description || null;
+  object.markup = options.markup || null;
+
+  return object;
+}
+
+/**
+ * For the given style file, read the contents and return the stardoc blocks as
+ * an array.
+ * @return {Array}
+ */
+function getStardocBlocks(filePath, cb) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    // Find comment blocks starting with <slash><star><star> and
+    // ending with <star><slash>.
+    var blocks = data.match(/^(\/\*\*[\s\S]*?\*\/)/gm);
+
+    if (blocks === null) {
+      cb(null, null);
+      return;
+    }
+
+    async.map(blocks, parseCommentBlock, (err, results) => {
+      if (err) {
+        cb(err);
+        return;
+      }
+
+      var dirname = path.dirname(filePath);
+
+      // Filter out null results.
+      results = results.filter(result => !!result);
+
+      async.map(results, readMarkup.bind(null, dirname), (err, results) => {
+        if (err) {
+          cb(err);
+          return;
+        }
+
+        cb(null, results);
+      });
+    });
+  });
+}
+
+
+function readMarkup(dirname, stardocBlock, cb) {
+  if (!stardocBlock.parameters.markup) {
+      cb(null, stardocBlock);
+      return;
+  }
+
+  var markupPath = path.resolve(
+    path.join(dirname, stardocBlock.parameters.markup)
+  );
+
+  fs.stat(markupPath, (err, stats) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    if (stats.isFile() === false) {
+      cb('Error: "' + markupPath + '" is not a file');
+      return;
+    }
+
+    fs.readFile(markupPath, 'utf8', (err, data) => {
+      if (err) {
+        cb('Error: Could not read "' + filePath + '": ' + err);
+        return;
+      }
+
+      stardocBlock.markup = data;
+
+      cb(null, stardocBlock);
+    });
+  });
+}
+
+/**
+ * Parse the given comment block and return or null if the block is not a valid
+ * stardoc comment.
+ */
+function parseCommentBlock(block, cb) {
+  var lines = block.split(/[\r\n|\n]/);
+
+  // Verify that the comment block begins with <slash><star><star>.
+  if (lines.shift() !== '/**') {
+    cb(null, null);
+    return;
+  }
+
+  // Verify that the comment block end with <space><star><slash>.
+  if (lines.pop() !== ' */') {
+    cb(null, null);
+    return;
+  }
+
+  // Verify that each line begins with <space><star> and an optional <space>
+  // and strip those characters.
+  var rLineStart = / \* ?/;
+  for (var i = 0; i < lines.length; i++) {
+    if (rLineStart.test(lines[i]) === false) {
+      cb(null, null);
+      return;
+    }
+
+    lines[i] = lines[i].replace(rLineStart, '');
+  }
+
+  // Find the line where the parameter block begins.
+  var parametersStartIndex = 0;
+  for (var i = lines.length - 1; i >= 0; i--) {
+    if (lines[i].indexOf('@') !== 0) {
+      parametersStartIndex = i + 1;
+      break;
+    }
+  }
+
+  // Concatenate the top description block. All lines above the parameter block
+  // is part of the description.
+  var description = '';
+  for (var i = 0; i < parametersStartIndex; i++) {
+    description += lines[i] + '\n';
+  }
+
+  // Parse the "@key value" parameters.
+  var parameters = {};
+  for (var i = parametersStartIndex; i < lines.length; i++) {
+    var tokens = lines[i].match(/^@([^\s]+) (.+)$/);
+    var key = tokens[1];
+    var value = tokens[2];
+
+    parameters[key] = value;
+  }
+
+  // Verify that the "id" key exist, which is the only required key.
+  if (parameters.id === 'undefined') {
+    cb(null, null);
+  }
+
+  var key = parameters.id.split('/').pop();
+
+  cb(null, {
+    key: key,
+    description: description,
+    parameters: parameters
+  });
+}
+
+/**
+ * @return {Array}
+ */
+function getMarkup(file, cb) {
+  fs.readFile(file, 'utf8');
+}
